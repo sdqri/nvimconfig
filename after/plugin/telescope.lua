4d514440b25d758854cd658905b60b83770eddb7
sadiq <sadiq>
2023-11-13 12:34:23 +0330

    FIX: Move telescope live_grep to telescope configs

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+-- Add live_grep
+vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
