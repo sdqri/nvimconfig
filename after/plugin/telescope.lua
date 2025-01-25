f30a552156465fad539de36b5c377f7f031f2b5e
sadiq <sadeg.r1@gmail.com>
2025-01-25 12:18:13 +0330

    FEAT: Add key bindings for buffers and help tags in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,3 +3,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
 -- Add live_grep
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+
+-- Add find buffers
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+
+-- Add find help
+vim.keymap.set("n", "<leader>fh", builtin.help_tags)
