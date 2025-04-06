47636d547b1ed71d9eef729f4108f1659372f21d
sadiq <sadeg.r1@gmail.com>
2025-04-06 11:36:56 +0330

    FEAT: Add luacheck as lua linter

--- lua/sadiq/plugins/linter.lua
+++ lua/sadiq/plugins/linter.lua
@@ -11,6 +11,9 @@ Plugin.opts = {
 function Plugin.config(_, opts)
     local lint = require("lint")
 
+    -- Apply the formatters_by_ft options
+    lint.linters_by_ft = opts.formatters_by_ft
+
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
@@ -23,6 +26,17 @@ function Plugin.config(_, opts)
     vim.keymap.set("n", "<leader>l", function()
         lint.try_lint()
     end, { desc = "Trigger linting for current file" })
+
+    -- 👇 I added this for detecting which linter current buffer uses
+    vim.api.nvim_create_user_command("ShowLinter", function()
+        local ft = vim.bo.filetype
+        local linters = lint.linters_by_ft[ft]
+        if linters then
+            print("Linter(s) for " .. ft .. ": " .. table.concat(linters, ", "))
+        else
+            print("No linter configured for filetype: " .. ft)
+        end
+    end, {})
 end
 
 return Plugin
