304b79ca563c89ac1448832355b324103e84745e
sadiq <sadeg.r1@gmail.com>
2025-04-08 10:28:31 +0330

    FEAT: Add prettier for js and ts

--- lua/sadiq/plugins/formatter.lua
+++ lua/sadiq/plugins/formatter.lua
@@ -1,23 +1,51 @@
 local Plugin = { "stevearc/conform.nvim" }
 
-Plugin.event = { "BufReadPre", "BufNewFile" }
+Plugin.event = { "BufReadPre", "BufNewFile", "BufWritePre" }
 
 Plugin.opts = {
     formatters_by_ft = {
         lua = { "stylua" },
         go = { "gofmt" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
     },
+
     format_on_save = {
         lsp_format = "fallback",
         timeout_ms = 500,
     },
 
     -- Set the log level. Use `:ConformInfo` to see the location of the log file.
-    log_level = vim.log.levels.ERROR,
+    log_level = vim.log.levels.TRACE,
     -- Conform will notify you when a formatter errors
     notify_on_error = true,
     -- Conform will notify you when no formatters are available for the buffer
     notify_no_formatters = true,
+    -- Custom formatters and overrides for built-in formatters
+
+    -- Custom formatters and overrides for built-in formatters
+    formatters = {
+        global_prettier = {
+            command = "sh",
+            args = {
+                "-c",
+                [[
+                        output=$(prettier "$1" 2>&1)
+                        code=$?
+                        echo "$output"
+                        exit $code
+                        ]],
+                "--",
+                "$FILENAME",
+            },
+            exit_codes = { 0 },
+            condition = function(_, ctx)
+                return vim.fn.filereadable(ctx.filename) == 1
+            end,
+            env = false,
+            inherit = false,
+        },
+    },
 }
 
 function Plugin.config(_, opts)
@@ -26,7 +54,7 @@ function Plugin.config(_, opts)
     -- Set keymap for normal and visual mode
     vim.keymap.set({ "n", "v" }, "<leader>f", function()
         -- Call the conform format function
-        require("conform").format({ async = false })
+        require("conform").format({ async = true })
     end, { desc = "Format the current buffer or selected range" })
 end
 
